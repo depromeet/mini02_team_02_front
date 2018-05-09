9e31b99a5d80779fa85a38cec6dde92ac9f1e026
권재원 <springkjw@gmail.com>
2018-05-09 23:08:00 +0900

    프로젝트 설정

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <div>
+  </div>
+  <div>
+    <button>마이페이지</button>
+    <button>주인을 찾아요</button>
+  </div>
+</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+</style>
